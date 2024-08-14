a89b9444a3ed29cdbb37412d8d289c63d8a3dc49
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-08-14 10:34:30 -0300

    Add `BuchbergerIncremental` and `inIdeal`

--- src/Buchberger.h
+++ src/Buchberger.h
@@ -76,3 +76,71 @@ pair<vector<Poly<K, ord>>, bool> Buchberger(vector<Poly<K, ord>> G, size_t max_i
   return {G, max_iter == 0};
 }
 
+template<typename K, class ord = LexOrd>
+struct BuchbergerIncremental {
+  vector<Poly<K, ord>> G;
+  queue<tuple<Amb, size_t, size_t>> ambs;
+  size_t t = 0;
+
+  BuchbergerIncremental(const vector<Poly<K, ord>>& GG) : G(GG) {
+    for (size_t j = 0; j < G.size(); j++) {
+      for (size_t i = 0; i <= j; i++) {
+        for (const auto& amb : ambiguities(G[i].lm(), G[j].lm())) {
+          ambs.push({amb, i, j});
+        }
+      }
+    }
+  }
+
+  optional<Poly<K, ord>> next() {
+    if (t < G.size()) {
+      Poly<K, ord> res = G[t];
+      t++;
+      return res;
+    }
+    while (!ambs.empty()) {
+      auto [amb, i, j] = ambs.front();
+      ambs.pop();
+
+      Poly<K, ord> s = S_poly(amb, G[i], G[j]);
+      s = reduce(s, G);
+
+      if (!s.isZero()) {
+        G.push_back(s);
+        for (size_t k = 0; k < G.size() - 1; k++) {
+          for (const auto& amb : ambiguities(G[k].lm(), s.lm())) {
+            ambs.push({amb, k, G.size() - 1});
+          }
+        }
+        t++;
+        return s;
+      }
+    }
+
+    return {};
+  }
+};
+
+enum IdealMembershipStatus {
+  InIdeal,
+  NotInIdeal,
+  Unknown
+};
+
+template<typename K, class ord = LexOrd>
+IdealMembershipStatus inIdeal(const vector<Poly<K, ord>>& G, Poly<K, ord> f, size_t max_sz = 20) {
+  BuchbergerIncremental bi(G);
+
+  for (size_t i = 0; i <= max_sz; i++) {
+    optional<Poly<K, ord>> p = bi.next();
+    if (!p.has_value()) {
+      return NotInIdeal;
+    }
+    f = reduce(f, bi.G);
+    if (f.isZero()) {
+      return InIdeal;
+    }
+  }
+
+  return Unknown;
+}
