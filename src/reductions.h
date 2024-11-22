5bf8f7b0de2735f808c1abadc943cd3f6941600d
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2024-11-22 19:41:24 -0300

    Use order from smaller to greater in reduction

--- src/reductions.h
+++ src/reductions.h
@@ -41,9 +41,16 @@ Poly<K, ord> reduce(Poly<K, ord> f, const Poly<K, ord>& g) {
 /* Reduce in place with a vector of polynomials */
 template<typename K, class ord = DegLexOrd>
 void reduce(Poly<K, ord>& f, const vector<Poly<K, ord>>& G) {
+  vector<size_t> idxs(G.size());
+  iota(idxs.begin(), idxs.end(), 0ll);
+  sort(idxs.begin(), idxs.end(), [&](size_t i, size_t j) {
+    return PolyOrd<K, ord>()(G[i], G[j]);
+  });
+
   while (true) {
     bool red = false;
-    for (const auto& g : G) {
+    for (size_t i : idxs) {
+      const Poly<K, ord>& g = G[i];
       Poly<K, ord> f_ = reduce(f, g);
       if (f != f_) {
         f = f_;
@@ -57,12 +64,20 @@ void reduce(Poly<K, ord>& f, const vector<Poly<K, ord>>& G) {
   }
 }
 
-/* Reduce in place with a vector of polynomials, but only with the nor marked */
+/* Reduce in place with a vector of polynomials, but only with the not marked */
 template<typename K, class ord = DegLexOrd>
 void reduce(Poly<K, ord>& f, const vector<Poly<K, ord>>& G, const vector<bool> marks) {
+  vector<size_t> idxs;
+  for (size_t i = 0; i < G.size(); i++) if (!marks[i]) {
+    idxs.push_back(i);
+  }
+  sort(idxs.begin(), idxs.end(), [&](size_t i, size_t j) {
+    return PolyOrd<K, ord>()(G[i], G[j]);
+  });
+
   while (true) {
     bool red = false;
-    for (size_t i = 0; i < G.size(); i++) if (!marks[i]) {
+    for (size_t i : idxs) {
       Poly<K, ord> f_ = reduce(f, G[i]);
       if (f != f_) {
         f = f_;
